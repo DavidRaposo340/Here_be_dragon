b01d6b74a536dfc82a2410c7ce8bd0bd0e8fdf87
DavidRaposo340 <up201806281@fe.up.pt>
2021-11-24 14:53:08 +0000

    versao2 (dragon's bane)

---
+++ miniquest2/src/dkeep/logic/exit.java
@@ -0,0 +1,13 @@
+package dkeep.logic;
+
+
+public class exit extends element {
+
+        
+    public exit(int row, int col, char symbol) {
+        super(row, col, 'E');
+    }
+    
+
+
+}
---
+++ miniquest2/src/dkeep/logic/sword.java
@@ -0,0 +1,11 @@
+package dkeep.logic;
+
+public class sword extends element {
+    //mais à frente vai se chamar sword
+    public sword(int row, int col, char symbol) {
+        super(row, col, symbol);
+    }
+
+        
+    
+}
